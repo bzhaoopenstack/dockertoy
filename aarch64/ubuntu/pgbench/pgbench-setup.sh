b012148f7e947e2017134c90babc2b7e284b19ee
bzhao <bzhaojyathousandy@gmail.com>
2020-12-26 11:17:09 +0800

    Update pgbench-setup.sh

--- aarch64/ubuntu/pgbench/pgbench-setup.sh
+++ aarch64/ubuntu/pgbench/pgbench-setup.sh
@@ -23,28 +23,29 @@ apt-get -q update \
 #yum -y install gcc gcc-c++ automake zlib zlib-devel bzip2 bzip2-devel bzip2- libs readline readline-devel bison gmp gmp-devel mpfr mpfr-devel libmpc libmpc-devel 
 apt-get install zlib1g zlib1g-dev bzip2 libbz2-dev readline-common libreadline-dev bison libgmp-dev libmpfr-dev libmpc-dev -y
 apt-get install flex -y
-useradd -m -d /home/greenplum -s /bin/bash greenplum && echo greenplum:greenplum | chpasswd && adduser greenplum sudo
-echo "greenplum ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+useradd -m -d /home/pgsql -s /bin/bash pgsql && echo pgsql:pgsql | chpasswd && adduser pgsql sudo
+echo "pgsql ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 
 ## pguser
 cd
+mkdir code
+cd code
 git clone https://github.com/postgres/postgres.git
 cd postgres
-./configure --prefix=/home/greenplum/pg-install/ ; make -j ; make install
-cd ../
+./configure --prefix=$HOME/pg-install/ ; make -j ; make install
+cd
 git clone https://github.com/mysqlonarm/benchmark-suites.git
 ls
 cd benchmark-suites/
-export PATH=$PATH:/home/greenplum/pg-install/bin
+export PATH=$PATH:$HOME/pg-install/bin
 which psql
 psql --version
-mkdir -p /home/greenplum/pgdata
-initdb --pgdata=/home/greenplum/pgdata --encoding=UTF8
-pg_ctl -D /home/greenplum/pgdata -l /home/greenplum/logfile start
-createdb -O greenplum psql
-
-createdb -O greenplum arm
-createdb -O greenplum postgres || true
-createdb -O greenplum greenplum
+mkdir -p $HOME/pgdata
+initdb --pgdata=$HOME/pgdata --encoding=UTF8
+pg_ctl -D $HOME/pgdata -l $HOME/logfile start
+createdb -O pgsql psql
+createdb -O pgsql arm
+createdb -O pgsql postgres || true
+createdb -O pgsql pgsql
 ./vm.sh
