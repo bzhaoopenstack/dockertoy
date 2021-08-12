3bd4f09fb5674e50a0afa862578efa3f98a1a882
bzhao <bzhaojyathousandy@gmail.com>
2021-08-12 09:17:11 +0800

    Create monitor_cmd.sh

---
+++ monitor_cmd.sh
@@ -0,0 +1,12 @@
+# 监控内存使用
+# https://blog.csdn.net/renfufei/article/details/105851728
+free -m
+# 每隔一秒高亮显示内存的变化情况
+watch -n 1 -d 'free -m'
+
+# cpu使用情况
+# top   1模式
+
+
+# linux：查看内核锁
+# https://blog.csdn.net/scdxmoe/article/details/39027781
